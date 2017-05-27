4653f14c6e26e84eacf833f7638941e373afdf6b
Lâm Phan Ngọc <davidbull931997@gmail.com>
2017-05-27 07:23:44 +0700

    socket.io

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+
+//declare socket.io to use at bin/www (without server listen)
+app.io = require('socket.io')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var CCUs = [];
+
+//socket.io on client connect event
+app.io.on('connection', function (socket) {
+  console.log('a user connected - socketId: ' + socket.id);
+
+  socket.on('client-send-reg-info', function (data) {
+    if (CCUs.indexOf(data) >= 0) {
+      socket.emit('server-send-reg-result', false);
+    } else {
+      CCUs.push(data);
+      socket.emit('server-send-reg-result', true);
+      app.io.sockets.emit('server-send-updated-cculist', CCUs);
+    }
+  });
+
+  socket.on('client-send-logout', function (data) {
+    CCUs.splice(CCUs.indexOf(data), 1);
+    socket.broadcast.emit('server-broadcast-logout-info', CCUs);
+  });
+
+  socket.on('client-send-chat-msg', function (data) {
+    app.io.sockets.emit('server-broadcast-chat-msg', data);
+  });
+
+  //Exit nodeJS event
+  process.on('exit', code => {
+    app.io.sockets.emit('server-on-close');
+    Object.keys(app.io.sockets.sockets).forEach(function (s) {
+      app.io.sockets.sockets[s].disconnect(true);
+    });
+    app.io.close();
+    process.exit (code);
+  });
+
+  // Catch CTRL+C
+  process.on('SIGINT', () => {
+    app.io.sockets.emit('server-on-close');
+    Object.keys(app.io.sockets.sockets).forEach(function (s) {
+      app.io.sockets.sockets[s].disconnect(true);
+    });
+    app.io.close();
+    process.exit (0);
+  });
+});
+
+module.exports = app;
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,142 @@
+//socket.io script
+var socket = io('http://127.0.0.1:3000');
+socket.on('server-send-reg-result', function (data) {
+    if (!data) {
+        swal('Something wrong', 'The username has been used!', 'error');
+    } else {
+        $('#reg-page').fadeOut();
+        setTimeout(function () {
+            $('#chat-page').fadeIn();
+            $('#ccu').height(($(window).height() - $('div#logout').height()));
+            $('#chatbox').height(($(window).height() - $('div#logout').height()));
+            $('#chatlog').height($('#chatbox').height() * 95 / 100);
+            $('#inputform').height($('#chatbox').height() * 5 / 100);
+        }, 401);
+    }
+});
+
+socket.on('server-send-updated-cculist', function (data) {
+    $('#ccu > p').each(function (index, element) {
+        if (index != 0) {
+            $(element).remove();
+        }
+    });
+    $.each(data, function (index, item) {
+        $('#ccu').append('<p class="text-center bg-info" style="padding: 10px 0;border: solid 1px silver;border-radius:2px;margin-bottom:0px;">' + item + '</p>');
+    });
+});
+
+socket.on('server-broadcast-logout-info', function (data) {
+    $('#ccu > p').each(function (index, element) {
+        if (index != 0) {
+            $(element).remove();
+        }
+    });
+    $.each(data, function (index, item) {
+        $('#ccu').append('<p class="text-center bg-info" style="padding: 10px 0;border: solid 1px silver;border-radius:2px;margin-bottom:0px;">' + item + '</p>');
+    });
+});
+
+socket.on('server-broadcast-chat-msg', function (data) {
+    $('#chatlog').append('<p style="padding: 5px 0;margin:0 0 0 10px;font-size:25px;">' + data.user + ': ' + data.msg + '</p>');
+});
+
+socket.on('server-on-close', function () {
+    socket.close();
+    swal('Disconnected', 'Disconnected from server, the server was offline', 'error').then(function () {
+        window.location.href = 'about:blank';
+    });
+});
+
+$(function () {
+    $('#reg-username').tooltip({ trigger: 'manual' });
+    $('[data-toggle="tooltip"]').tooltip({ trigger: 'manual' });
+    $(window).on('beforeunload', function () {
+        socket.emit('client-send-logout', $('#currentUserName').text());
+    });
+
+
+    //reg page
+    $('#reg-btn').click(function () {
+        if ($('#reg-username').val() == '') {
+            $('#reg-username').tooltip('show');
+            setTimeout(function () {
+                $('#reg-username').tooltip('hide');
+            }, 2000);
+        }
+        else {
+            socket.emit('client-send-reg-info', $('#reg-username').val());
+            $('#currentUserName').text($('#reg-username').val());
+            $('#msg-input').focus();
+            $('#chatlog').html('');
+        }
+        return false;
+    });
+
+    $("#reg-username").on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            if ($('#reg-username').val() == '') {
+                $('#reg-username').tooltip('show');
+                setTimeout(function () {
+                    $('#reg-username').tooltip('hide');
+                }, 2000);
+            }
+            else {
+                socket.emit('client-send-reg-info', $('#reg-username').val());
+                $('#currentUserName').text($('#reg-username').val());
+                $('#msg-input').focus();
+            }
+            return false;
+        }
+    });
+
+    //chatting page
+    $('#logout-btn').click(function () {
+        $('#chat-page').fadeOut();
+        setTimeout(function () {
+            $('#reg-page').fadeIn();
+        }, 401);
+        socket.emit('client-send-logout', $('#currentUserName').text());
+        return false;
+    });
+
+    $('#msg-btn').click(function () {
+        if ($('#msg-input').val() == '') {
+            $('[data-toggle="tooltip"]').tooltip('show');
+            setTimeout(function () {
+                $('[data-toggle="tooltip"]').tooltip('hide');
+            }, 2000);
+        } else {
+            socket.emit('client-send-chat-msg', { user: $('#currentUserName').text(), msg: $('#msg-input').val() });
+            $('#msg-input').val('');
+        }
+        return false;
+    });
+
+    $("#msg-input").on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            if ($('#msg-input').val() == '') {
+                $('[data-toggle="tooltip"]').tooltip('show');
+                setTimeout(function () {
+                    $('[data-toggle="tooltip"]').tooltip('hide');
+                }, 2000);
+            } else {
+                socket.emit('client-send-chat-msg', { user: $('#currentUserName').text(), msg: $('#msg-input').val() });
+                $('#msg-input').val('');
+            }
+            return false;
+        }
+
+    });
+
+    $('#ccu').height(($(window).height() - $('div#logout').height()));
+    $('#chatbox').height(($(window).height() - $('div#logout').height()));
+    $('#chatlog').height($('#chatbox').height() * 95 / 100);
+    $('#inputform').height($('#chatbox').height() * 5 / 100);
+    $(window).resize(function () {
+        $('#ccu').height(($(window).height() - $('div#logout').height()));
+        $('#chatbox').height(($(window).height() - $('div#logout').height()));
+        $('#chatlog').height($('#chatbox').height() * 95 / 100);
+        $('#inputform').height($('#chatbox').height() * 5 / 100);
+    });
+});

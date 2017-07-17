0f53f13a1ce874011f0156899af5b1695b51f02f
Lâm Phan Ngọc <davidbull931997@gmail.com>
2017-07-17 12:27:16 +0700

    redirect http to https

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.use((req, res, next) => {
 
 app.use('/', index);
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production' &&
+    req.headers['x-forwarded-proto'] !== 'https')
+    return res.redirect(['https://', req.hostname, req.url].join(''));
+  return next();
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
